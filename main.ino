eb0ab96c9a9c9bfc7498fadcf101a891af1d0e1b
Leandro Vandari <90213288+LeandroVandari@users.noreply.github.com>
2025-08-15 16:13:32 -0300

    dht22

--- main.ino
+++ main.ino
@@ -1,13 +1,25 @@
 #include <SPI.h> // Serial Peripheral Interface --> comunicação com os periferais
+#include "DHT.h"
 
-const int PRESSURE_PIN = A0;
+DHT dht(2, DHT22);
+
+const int PRESSURE_PIN = A1;
 
 void setup() {
   Serial.begin(9600);
+  dht.begin();
   Serial.println("Terminei o setup");
+ // SPI.begin();
+ // SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float hum = dht.readHumidity();
+  float temp = dht.readTemperature();
+  Serial.println(hum);
+  Serial.println(temp);
 
+  Serial.println("   ");
+  delay(1000);
 }
