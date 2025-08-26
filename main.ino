2408c3d1963cccf9298d5cc367ba51cd5e2830e2
Leandro Vandari <90213288+LeandroVandari@users.noreply.github.com>
2025-08-26 15:33:33 -0300

    add bmp 180

--- main.ino
+++ main.ino
@@ -1,13 +1,16 @@
 #include <SPI.h> // Serial Peripheral Interface --> comunicação com os periferais
 #include "DHT.h"
+#include <Adafruit_BMP085.h>
 
 DHT dht(2, DHT22);
+Adafruit_BMP085 bmp;
 
 const int PRESSURE_PIN = A1;
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  bmp.begin();
   Serial.println("Terminei o setup");
  // SPI.begin();
  // SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));
@@ -17,8 +20,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   float hum = dht.readHumidity();
   float temp = dht.readTemperature();
-  Serial.println(hum);
-  Serial.println(temp);
+  float pressure = bmp.readPressure();
+  Serial.println("Umidade: " +String(hum) + "%");
+  Serial.println("Temperatura: "+String(temp) + "°C");
+  Serial.println("Pressão: "+String(pressure) + "Pa");
 
   Serial.println("   ");
   delay(1000);
